02ed2253bfbde0cded8020ca77bca91502455132
Turgut Kurt <turgut@softcand.com>
2022-08-11 13:52:48 +0300

    added loader

--- template/src/assets/index.js
+++ template/src/assets/index.js
@@ -5,3 +5,4 @@ export {default as Home} from './svg/bottomTabBar/Home';
 export {default as Swiper1} from './svg/swiper/Swiper1';
 export {default as Swiper2} from './svg/swiper/Swiper2';
 export {default as Swiper3} from './svg/swiper/Swiper3';
+export {default as loader} from './lottie/loader.json';
--- template/src/screens/Tab4/index.js
+++ template/src/screens/Tab4/index.js
@@ -1,12 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
 
+import {Loader} from '~/components';
 import React from 'react';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const Tab4 = props => {
+  const insets = useSafeAreaInsets();
   const {Container} = styles;
   return (
-    <View style={Container}>
+    <View style={[Container, {paddingTop: insets.top}]}>
       <Text>Tab4</Text>
+      <Loader loading={true} />
     </View>
   );
 };
